5b0dd05e125475cfa5f97a5bb55185352e320553
I1Va <mai.abrutinid@gmil.com>
2024-10-21 17:52:23 +0300

    created output module

---
+++ src/output_funcs.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "assert.h"
+#include <string.h>
+
+#include "output_funcs.h"
+
+void fprintf_border(FILE* stream, const char bord_char, const size_t bord_sz, bool new_line) {
+    for (size_t i = 0; i < bord_sz; i++) {
+        fprintf(stream, WHT);
+        fputc(bord_char, stream);
+    }
+    if (new_line) {
+        fputc('\n', stream);
+    }
+}
+
+void fprintf_title(FILE *stream, const char tittle[], const char bord_char, const size_t bord_sz) {
+    assert(tittle != NULL);
+    size_t tittle_sz = strlen(tittle);
+    if (bord_sz < tittle_sz) {
+        return;
+    }
+    size_t len = bord_sz - tittle_sz;
+    fprintf_border(stream, bord_char, len / 2, false);
+    fprintf_red(stream, "%s", tittle);
+    fprintf_border(stream, bord_char, (len + 1) / 2, true);
+}
